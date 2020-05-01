2226d0d7e2aa54ce15b98e090be2b4615cea127e
Nazaré Lisboa <zaza.naza95@gmail.com>
2020-05-01 15:39:43 -0300

    created socialLinks component

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/MariaLisboa1",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/mnlisboa_",
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com/nalisb",
+  },
+  {
+    label: "Linkedin",
+    url: "https://www.linkedin.com/in/maria-lisboa-b5b221137/",
+  },
+]
+
+export default links
