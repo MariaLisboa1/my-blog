f458adf326e708968931c3935f6723e96c836d4b
MariaLisboa1 <zaza.naza95@gmail.com>
2020-05-19 11:03:10 -0300

    paging logic

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,50 +1,72 @@
 const path = require("path")
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const {createFilePath} = require(`gatsby-source-filesystem`)
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  // Ensures we are processing only markdown files
-  if (node.internal.type === "MarkdownRemark") {
-    // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
-    const slug = createFilePath({
-      node,
-      getNode,
-      basePath: "pages",
-    })
+exports.onCreateNode = ({node, getNode, actions}) => {
+    const {createNodeField} = actions
+    // Ensures we are processing only markdown files
+    if (node.internal.type === "MarkdownRemark") { // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
+        const slug = createFilePath({node, getNode, basePath: "pages"})
 
-    // Creates new query'able field with name of 'slug'
-    createNodeField({
-      node,
-      name: "slug",
-      value: `/${slug.slice(12)}`,
-    })
-  }
+        // Creates new query'able field with name of 'slug'
+        createNodeField({
+                node, name: "slug", value: `/${
+                slug.slice(12)
+            }`
+        })
+    }
 }
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+exports.createPages = ({graphql, actions}) => {
+    const {createPage} = actions
 
-  return graphql(`
+    return graphql(`
     {
-      allMarkdownRemark {
+      allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
         edges {
           node {
             fields {
               slug
             }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
           }
         }
       }
     }
   `).then(results => {
-    results.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.fields.slug,
-        component: path.resolve("./src/templates/blog-post.js"),
-        context: {
-          slug: node.fields.slug,
-        },
-      })
+        const posts = results.data.allMarkdownRemark.edges;
+        posts.forEach(({node}) => {
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve("./src/templates/blog-post.js"),
+                context: {
+                    slug: node.fields.slug
+                }
+            })
+        })
+
+        const postsPerPage = 6;
+        const numPages = Math.ceil(posts.length)
+
+        Array.from({length: numPages}).forEach((_, index) => {
+            createPage({
+                path: index === 0 ? `/` : `/page/${
+                    index + 1
+                }`,
+                component: path.resolve("./src/templates/blog-list.js"),
+                context: {
+                    limit: postsPerPage,
+                    skip: index * postsPerPage,
+                    numPages,
+                    currentPage: index + 1
+                }
+            })
+        })
     })
-  })
 }
